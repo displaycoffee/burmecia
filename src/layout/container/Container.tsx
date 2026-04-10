a3c4b65ebfb4646dbdf8410d5dbba823fa89441c
displaycoffee <adria.m.murphy@gmail.com>
2026-04-10 10:46:23 -0600

    format and a few other changes

--- src/layout/container/Container.tsx
+++ src/layout/container/Container.tsx
@@ -11,7 +11,6 @@ import { useBodyClass, useRespond } from '../../_config/scripts/hooks';
 /* Local components */
 import { Context } from '../../context/Context';
 import { ErrorBoundary } from '../../components/error-boundary/ErrorBoundary';
-import { IconMap } from '../../components/icons/Icons';
 import { Navigation } from '../../components/navigation/Navigation';
 import { Slideout, SlideoutOverlay } from '../../components/slideout/Slideout';
 import { Header } from '../header/Header';
@@ -49,8 +48,6 @@ export const Container = () => {
 	return (
 		<div className="container">
 			<ErrorBoundary message={<ContainerError />}>
-				<IconMap />
-
 				<SlideoutOverlay options={slideoutOptions} />
 
 				<Header />
