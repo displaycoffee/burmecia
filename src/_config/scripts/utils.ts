c7c39336dddeeaf186f89e46cb5116e82b739ba1
displaycoffee <adria.m.murphy@gmail.com>
2026-04-20 08:10:01 -0600

    updates

--- src/_config/scripts/utils.ts
+++ src/_config/scripts/utils.ts
@@ -1,7 +1,3 @@
-/* React */
-import { ReactNode } from 'react';
-import { createRoot } from 'react-dom/client';
-
 export const utils = {
 	getLast: (value: string | [], delimeter?: string) => {
 		// Get last item in array
@@ -36,17 +32,6 @@ export const utils = {
 			// Observe to toggle sticky class
 			const stickyObserver = new IntersectionObserver(([e]) => stickyCallback(e), stickyOptions);
 			stickyObserver.observe(element);
-		}
-	},
-	renderTarget: (element: string, component: ReactNode) => {
-		// Render target for app
-		const targetElement = document.querySelector(element);
-		if (targetElement) {
-			const targetHasChildren = targetElement.children.length > 0;
-			if (!targetHasChildren) {
-				const target = createRoot(targetElement);
-				target.render(component);
-			}
 		}
 	},
 	scrollTo: (e: EventsType, selector: string | undefined, offset: number) => {
