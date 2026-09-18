4f8bf161dd876b2ad7677eec9c33f8c10f642526
Adria <displaycoffee@users.noreply.github.com>
2026-09-18 09:31:12 -0600

    Update vite.sitemap.js

--- vite.sitemap.js
+++ vite.sitemap.js
@@ -11,6 +11,7 @@ let sitemap = {
 	exclude: ['/assets', '/assets/css', '/assets/fonts', '/assets/images', '/assets/images/test', '/assets/images/theme', '/assets/js'],
 	dynamicRoutes: ['/page-one', '/page-two', '/page-two/child-page-one', '/page-two/child-page-two'],
 };
+
 if (location?.pathname && location.pathname != '/') sitemap.basePath = location.pathname;
 
 export const sitemapConfig = sitemap;
